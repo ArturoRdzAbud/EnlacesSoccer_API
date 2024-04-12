ccbd66dee339faafc2fd08e9537ab8e669ee0876
ArturoRdzAbud <arturo.rodriguez.abud@gmail.com>
2024-04-12 13:07:31 -0600

    se agregan servicios para guardar resultados

--- app/models/GuardarCapturaDeResultados.js
+++ app/models/GuardarCapturaDeResultados.js
@@ -9,8 +9,8 @@ exports.GuardarCapturaDeResultados = async (body) => {
       .input('pnIdLiga', body.pnIdLiga)
       .input('pnIdTorneo', body.pnIdTorneo)
       .input('pnIdJornada', body.pnIdJornada)
-      .input('pnIdEquipo', body.pnIdEquipo)
-      .input('psXmlEquipos', body.psXmlEquipos)
+      .input('psXmlResultados1', body.psXmlResultados1)
+      .input('psXmlResultados2', body.psXmlResultados2)
       .execute('GuardarCapturaDeResultados');
     return result.recordsets[0];
   } catch (err) {
