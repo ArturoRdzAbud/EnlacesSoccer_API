5e62bc2a1a56acb0c82c42b5513de2c51f8262ad
rolandoloera <roliclock@gmail.com>
2024-05-03 15:33:25 -0600

    fix

--- app/models/ConsultarCapturaDeResultados.js
+++ app/models/ConsultarCapturaDeResultados.js
@@ -3,6 +3,7 @@ const sqlConfig = require('../config/db');
 
 exports.ConsultarCapturaDeResultados = async (params) => {
     try {
+        // console.log('entra al try')
         const pool = await mssql.connect(sqlConfig);
         const result = await pool.request()
             .input('pnIdLiga', params.pnIdLiga)
@@ -11,6 +12,7 @@ exports.ConsultarCapturaDeResultados = async (params) => {
             .execute('ConsultarCapturaDeResultados');
         return result.recordsets[0];
     } catch (err) {
+        console.log('entra al control de errores', err.message)
         return err;
     }
 };
